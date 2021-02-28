4f49f5a5bd6ad14d31254abcd52ea29e55367e6d
Benjamin-Davies <bentendavies@gmail.com>
2021-03-01 11:35:17 +1300

    Add basic url generator

--- KamarCalendar/wwwroot/url-gen.js
+++ KamarCalendar/wwwroot/url-gen.js
@@ -2,21 +2,45 @@ const { createElement: e, useCallback, useState } = React;
 
 function UrlGen() {
   const [isMMC, setIsMMC] = useState(true);
+  const [portalAddress, setPortalAddress] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [icalAddress, setIcalAddress] = useState(null);
 
   const onChange = useCallback(({ target }) => {
     switch (target.name) {
       case 'school':
         setIsMMC(target.value === 'MMC');
         break;
+      case 'portalAddress':
+        setPortalAddress(target.value);
+        break;
+      case 'username':
+        setUsername(target.value);
+        break;
+      case 'password':
+        setPassword(target.value);
+        break;
     }
   });
 
   const onSubmit = useCallback(
-    (ev) => {
+    async (ev) => {
       ev.preventDefault();
-      console.log(isMMC);
+
+      try {
+        setIcalAddress(
+          await generateIcalAddress(
+            isMMC ? '_' : portalAddress,
+            username,
+            password
+          )
+        );
+      } catch (e) {
+        setIcalAddress(e.toString());
+      }
     },
-    [isMMC]
+    [isMMC, portalAddress, username, password]
   );
 
   return e(
@@ -51,8 +75,50 @@ function UrlGen() {
         'Another School'
       )
     ),
-    e('button', { type: 'submit' }, 'Generate URL')
+    !isMMC &&
+      e(
+        'p',
+        null,
+        e(
+          'label',
+          null,
+          'KAMAR Portal Address:',
+          e('input', { name: 'portalAddress', value: portalAddress, onChange })
+        )
+      ),
+    e(
+      'p',
+      null,
+      e(
+        'label',
+        null,
+        'Username:',
+        e('input', { name: 'username', value: username, onChange })
+      )
+    ),
+    e(
+      'p',
+      null,
+      e(
+        'label',
+        null,
+        'Password:',
+        e('input', {
+          name: 'password',
+          value: password,
+          onChange,
+          type: 'password',
+        })
+      )
+    ),
+    e('button', { type: 'submit' }, 'Generate URL'),
+    icalAddress &&
+      e('p', null, e('a', { href: icalAddress, target: '_blank' }, icalAddress))
   );
+}
+
+async function generateIcalAddress(portalAddress, username, password) {
+  return `${location.origin}/ICal/${portalAddress}/${username}/${password}`;
 }
 
 ReactDOM.render(e(UrlGen), document.getElementById('url-gen-root'));
